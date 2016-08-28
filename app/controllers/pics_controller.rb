3ad05535789ba110d64b0c162914c47a01225217
Priom Chowdhury <priom.galaxy@gmail.com>
2016-08-28 18:52:23 -0300

    CRUD action and views

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,7 +2,7 @@ class PicsController < ApplicationController
   before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
-    
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def show
@@ -22,12 +22,30 @@ class PicsController < ApplicationController
     end
   end
 
-  private
-  def pic_params
-    params.require(:pic).permit(:title, :description)
+  def edit
+    
   end
 
-  def find_pic
-    @pic = Pic.find(params[:id])
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Congrats! Pic was updated"
+    else
+      render 'edit'
+    end
   end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+
+
+  private
+    def pic_params
+      params.require(:pic).permit(:title, :description)
+    end
+
+    def find_pic
+      @pic = Pic.find(params[:id])
+    end
 end
